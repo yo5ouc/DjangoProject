b3bd4bed9dfa750dc3a7ceb74e7567cad8f3ca42
yo5ouc <yo5ouc@gmail.com>
2026-08-29 22:10:06 +0300

    Force add admin creation script

---
+++ core/create_admin.py
@@ -0,0 +1,21 @@
+import os
+import django
+
+# Setup Django configuration
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+django.setup()
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+# Change these to the username and password you want!
+username = 'admin'
+password = 'dfrt54DFG342**&;.,hgfrte'
+email = 'yo5ouc@gmail.com'
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print("Superuser created successfully!")
+else:
+    print("Superuser already exists.")
