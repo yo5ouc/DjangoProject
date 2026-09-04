f1f8b8fc7b3a6b32833f6185f479bcc9e03d0564
yo5ouc <yo5ouc@gmail.com>
2026-09-04 22:45:08 +0300

    Update WebSocket routing pattern to support dynamic station callsigns

--- core/routing.py
+++ core/routing.py
@@ -2,5 +2,7 @@ from django.urls import re_path
 from core import consumers
 
 websocket_urlpatterns = [
-    re_path(r'^ws/radio/$', consumers.RadioConsumer.as_asgi()),
+    re_path(r'^ws/radio/(?P<callsign>\w+)/?$', consumers.RadioConsumer.as_asgi()),
+    # Keep the fallback pattern below if you have any legacy tools connecting without a callsign
+    re_path(r'^ws/radio/?$', consumers.RadioConsumer.as_asgi()),
 ]
